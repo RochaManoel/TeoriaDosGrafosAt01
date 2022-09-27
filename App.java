29aad6db2c0f7fa7852613fa2a47e14a88a166f7
Manoel Rocha <113397148+RochaManoel@users.noreply.github.com>
2022-09-27 17:37:46 -0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 import Entities.Engine;
-//import Grafos.CenarioOne;
 
 public class App {
 
@@ -9,21 +8,17 @@ public class App {
         Scanner sc = new Scanner(System.in);
         boolean control = true;
         while(control){
-            //System.out.println("Digite o Cenário desejado:\n[0] - Fechar Programa\n[1] - Cenário 1\n[2] - Cenário 2");
-            //int option = sc.nextInt();
-            int option;
-            option = 2;
+            System.out.println("Digite o Cenário desejado:\n[0] - Fechar Programa\n[1] - Cenário 1\n[2] - Cenário 2");
+            int option = sc.nextInt();
             switch(option){
                 case 0:
                     control = false;
                     break;
                 case 1:
                     new Engine(true);
-                    control = false;
                     break;
                 case 2:
                     new Engine(false);
-                    control = false;
                     break;
                 default:
                     System.out.println("Comando Invalido");
